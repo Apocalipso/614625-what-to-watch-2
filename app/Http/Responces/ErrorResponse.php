8539c71c52bcf08f605b562a2eb519dc8b7a2985
Maksim <Pruchkin-Maksim@yandex.ru>
2023-04-17 00:06:06 +0300

    начало

---
+++ app/Http/Responces/ErrorResponse.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Responces;
+
+use Symfony\Component\HttpFoundation\Response;
+use App\Http\Responses\AbstractResponse;
+
+class ErrorResponse extends AbstractResponse
+{
+
+}
---
+++ app/Http/Responces/SuccessResponce.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Responces;
+
+use Symfony\Component\HttpFoundation\Response;
+use App\Http\Responses\AbstractResponse;
+class SuccessResponce extends AbstractResponse
+{
+
+}
